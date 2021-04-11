e530a7defef14df5120b4c0f8396b19fb73dd9da
Rubayet Alam <201828008@student.mist.ac.bd>
2021-04-11 19:31:44 +0600

    book exercise-2.1

--- simple_messages_2-2.py
+++ simple_messages_2-2.py
@@ -2,3 +2,5 @@ news="Today there's a record breaking corona death in Bangladesh"
 print(news)
 news="From 14 april ramadan will start"
 print(news)
+#both of them got printed
+#the values of variable "news" was not replaced.
