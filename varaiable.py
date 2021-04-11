7ebc5b6f96d2c58b0bdfdb44e5f1e9f55503c936
Rubayet Alam <201828008@student.mist.ac.bd>
2021-04-11 18:58:18 +0600

    variables 1

---
+++ varaiable.py
@@ -0,0 +1,17 @@
+a="rubayet"
+b=24
+c=45.757363
+d= True
+e= None
+#printing the variables
+print(1-a)
+print(b)
+print(c)
+print(d)
+print(e)
+#printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
