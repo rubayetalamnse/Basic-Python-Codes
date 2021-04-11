709bab5b3baf4085923031d97067ef6567488a0e
Rubayet Alam <201828008@student.mist.ac.bd>
2021-04-11 18:06:17 +0600

    use of os module
    os module to print the content of directory

--- bbfd.py
+++ bbfd.py
@@ -1,2 +1,8 @@
+#Author: Rubayet Alam
+#License: Nuclear Particles
+''' nuclear is 
+the 
+zero carbon
+ENERGY'''
 print ("hello")
 print("Rubayet Alam")
---
+++ name_files_in_folder.py
@@ -0,0 +1,4 @@
+import os
+print(os.listdir())
+'''this code will print all the files name, 
+which are present in the folder where the code is saved'''
