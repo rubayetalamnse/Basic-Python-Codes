3f52b23cdd60200df7678dfb7627e89bc3937b66
Rubayet Alam <201828008@student.mist.ac.bd>
2021-04-11 18:08:35 +0600

    using playsound

--- playsound.py
+++ playsound.py
@@ -1,2 +1,5 @@
 from playsound import playsound
 playsound('D:\\python codes\\darkside.mp3')
+# this code will not work in the github desktop folder for python
+# this code is used to plays sounds/audio files
+# we have used double backslash, cause one \ creates escape sequence
