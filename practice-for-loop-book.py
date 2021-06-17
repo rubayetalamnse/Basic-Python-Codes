6898f85013fd08a3874f9952769c3a5a1c819f01
Rubayet Alam <201828008@student.mist.ac.bd>
2021-06-17 20:45:05 +0600

    p

---
+++ practice-for-loop-book.py
@@ -0,0 +1,4 @@
+magicians = ['alice','david','carolina','harry']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick.")
+    print(f"can't wait to see another one, {magician.title()}.")
