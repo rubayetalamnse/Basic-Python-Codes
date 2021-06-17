10366a94e9a4c27903e6a8f61fd32de60006353b
Rubayet Alam <201828008@student.mist.ac.bd>
2021-06-17 21:51:19 +0600

    book exercise

---
+++ list-for-4.1-book.py
@@ -0,0 +1,4 @@
+languages = ['python','c','c++','matlab','javascript','java','julia','scala','swift']
+for language in languages:
+    print(f"{language.title()}, is really good!")
+print("I really love programming, mainly",languages[0])
