472503359b1baccc26fa52d5f036b63562b5d417
Rubayet Alam <201828008@student.mist.ac.bd>
2021-06-24 21:33:49 +0600

    Create basic-tuple2.py

---
+++ basic-tuple2.py
@@ -0,0 +1,14 @@
+#got to know something new:----------------
+
+#let's create a tuple----->
+
+numbers = (1,2,3,4,5,6,7,8,9,10)
+
+#reverse tuple ----
+b = numbers[::-1]
+print(b)
+#output---(10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+
+c = numbers[::2]
+print(c)
+#output--->(1, 3, 5, 7, 9)
